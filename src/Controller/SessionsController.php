c2c0ffb9f3b4050fb37889daabf4fb1cd41f0787
Rachel Marquant <marquantrachel@gmail.com>
2025-04-01 14:15:46 +0200

    Possibilité de supprimer un module d'une session

--- src/Controller/SessionsController.php
+++ src/Controller/SessionsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Intern;
+use App\Entity\Program;
 use App\Entity\Session;
 use App\Form\ProgramType;
 use App\Form\SessionType;
@@ -68,7 +69,7 @@ final class SessionsController extends AbstractController
         $intern = $internRepository->findBy(['id'=>$idIntern]);
         $session = $session[0];
         $intern = $session->removeIntern($intern[0]);
-        // dd($intern);
+    
         $entityManager->persist($intern);
         $entityManager->flush();
         return $this->redirectToRoute('detail_session', ['id'=> $session->getId()] );
@@ -107,27 +108,24 @@ final class SessionsController extends AbstractController
     {
         $session = $sessionRepository->findBy(['id'=> $idSession]);
         $session = $session[0]; // pour récuyperer la premiere et unique valeur du tableau donnée
-
         $course = $courseRepository->findAll();
-        $program = $programRepository->findAll();
-        $program = $program[0];
+        $program = new Program();
         
         $form = $this->createForm(ProgramType::class, $program);
         $form->handleRequest($request); 
         if ($form->isSubmitted() && $form->isValid()) {
             $program = $form->getData();
             $program->setSession($session);
-            // dd($program);
             
-            // on cherche si la session est deja dans un programme
-            $sessionTest = $programRepository->findBy(['session' => $session->getId()]);
-            $programTest = $programRepository->findBy(['course' => $program->getCourse()]);
+            // on cherche si le module est déjà dans la session
+            $programTest = $programRepository->findBy(['course' => $program->getCourse(), 'session' => $session->getId()]);
             // si on en trouve on retourne au formulaire et signale que le module existe déjà dedans
-            if (!($programTest == []) && !($sessionTest == [])){
+            
+            if ($programTest != []){ // si programTest n'est pas un tableau vide alors il existe deja
                 $error = "Le module existe déjà";
                 return $this->render('session/newProgram.html.twig', ['formProgram'=> $form, 'id'=> $session->getId(), 'error' => $error] );
             }
-            
+
             $entityManager->persist($program);
             $entityManager->flush();
 
@@ -135,6 +133,21 @@ final class SessionsController extends AbstractController
 
         }
         return $this->render('session/newProgram.html.twig', ['formProgram'=> $form, 'id'=> $session->getId(), 'error' => ''] );
+    }
+
+    #[Route('/session/{idSession}/delete/{idCourse}', name:'delete_course')]
+    public function delete_course ($idSession, $idCourse, CourseRepository $courseRepository,  ProgramRepository $programRepository, SessionRepository $sessionRepository, EntityManagerInterface $entityManager) : Response {
+        // $program = $programRepository->findBy(['id' => $idProgram]);
+        $session = $sessionRepository->findBy(['id'=>$idSession])[0]; 
+        $course = $courseRepository->findBy(['id'=>$idCourse])[0];
+        $program = $programRepository->findBy(['session'=>$session->getId(), 'course' => $course->getId()])[0];
+        // dd($program);
+        $program = $session->removeProgram($program);
+
+        $entityManager->persist($program);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('detail_session', ['id' =>$session->getId()]);
     }
 
     #[Route('/session/{id}', name:'detail_session')]
